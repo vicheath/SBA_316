ae3c4cfa0923fd2a77ad0ebc41177cb7c28a1650
vicheath <vicheath.a.s@gmail.com>
2023-12-14 14:49:11 -0500

    fourth commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const body = document.getElementById
+document.querySelector(".textbox_main").addEventListener("submit", function (event) {
+    event.preventDefault(); // prevent form submission
+    const textbox = document.getElementById("textbox");
+    if (textbox.value.trim() === "") {
+        alert("Please enter some text before submitting.");
+    } else {
+        const paragraph = document.createElement("p");
+        paragraph.textContent = textbox.value;
+        document.querySelector(".text_area").appendChild(paragraph);
+        textbox.value = ""; // clear the textbox
+    }
+});
