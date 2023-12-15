8195521933de633f9ced73ea841a502f405954c3
vicheath <vicheath.a.s@gmail.com>
2023-12-14 21:58:19 -0500

    fifth commit

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-document.querySelector(".textbox_main").addEventListener("submit", function (event) {
-    event.preventDefault(); // prevent form submission
-    const textbox = document.getElementById("textbox");
-    if (textbox.value.trim() === "") {
-        alert("Please enter some text before submitting.");
+
+document.body.style.backgroundColor = '#765898'
+
+var menuList = [
+    { text: "EVA 01", url: "https://github.com/vicheath", color: "#52d053" },
+    { text: "EVA 02", url: "http://127.0.0.1:5500/SBA/Gaming%20Page/index.html", color: "#c20f0f" },
+    { text: "EVA 03", url: "http://127.0.0.1:5500/SBA/Blogpost/index.html", color: "#a2b2df" },
+    { text: "NERV", url: "#", color: "#e6770b", disabled: true }
+   ];
+   
+   var navList = document.createElement("ul");
+   navList.setAttribute("class", "nav justify-content-center");
+   
+   menuList.forEach(function (item) {
+    var listItem = document.createElement("li");
+    listItem.setAttribute("class", "nav-item");
+   
+    var anchor = document.createElement("a");
+    anchor.setAttribute("class", "nav-link");
+    anchor.setAttribute("href", item.url);
+    anchor.setAttribute("target", "_blank");
+    anchor.setAttribute("style", "color: " + item.color + ";");
+    anchor.textContent = item.text;
+   
+    if (item.disabled) {
+       anchor.setAttribute("aria-disabled", "true");
+       anchor.setAttribute("class", "nav-link disabled");
     } else {
-        const paragraph = document.createElement("p");
-        paragraph.textContent = textbox.value;
-        document.querySelector(".text_area").appendChild(paragraph);
-        textbox.value = ""; // clear the textbox
+       anchor.setAttribute("aria-current", "page");
+       anchor.setAttribute("class", "nav-link active");
     }
-});
+   
+    listItem.appendChild(anchor);
+    navList.appendChild(listItem);
+   });
+
+   var menuButtonsContainer = document.getElementById("menu_buttons_container");
+
+   document.body.appendChild(navList);
+
+   
+
